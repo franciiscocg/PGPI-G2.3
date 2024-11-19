d1a537a402a6730bfb71026d33991f917b35ab4d
fracapgar1 <92797553+franciiscocg@users.noreply.github.com>
2024-11-19 21:24:36 +0100

    Revert "9 views pedido"

--- OfficePack/OfficePack/settings.py
+++ OfficePack/OfficePack/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'Producto',
     'Pedido',
     'Producto_pedido',
-    'users_app',
 ]
 
 MIDDLEWARE = [
@@ -107,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'es'
+LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
 
--- OfficePack/Pedido/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import Pedido
-
-
-class PedidoForm(forms.ModelForm):
-    class Meta:
-        model = Pedido
-        fields = ['email', 'importe', 'direccion', 'estado']
