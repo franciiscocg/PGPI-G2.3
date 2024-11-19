07e135c82dc1111205aa958fa6863c2d8bb5634d
franciiscocg <cisquitofrancisco@gmail.com>
2024-11-19 18:46:05 +0100

    añadido nuevas cosas se necesita comparar con rama cesta

--- OfficePack/OfficePack/urls.py
+++ OfficePack/OfficePack/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from users_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')),
+    path('', views.home, name='home'),
+    path('register/', views.register),
+    path('logout/', views.signout, name='signout'),
 ]
--- OfficePack/users_app/views.py
+++ OfficePack/users_app/views.py
@@ -1,15 +1,25 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login
-from .forms import CustomUserCreationForm
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import UserCreationForm
+from django.http import HttpResponse
 
 
 def register(request):
     if request.method == 'POST':
-        form = CustomUserCreationForm(request.POST)
+        form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)  # Inicia sesión automáticamente
             return redirect('home')  # Redirige a la página principal
     else:
-        form = CustomUserCreationForm()
+        form = UserCreationForm()
     return render(request, 'register.html', {'form': form})
+
+
+def signout(request):
+    logout(request)
+    return redirect('home')
+
+
+def home(request):
+    return render(request, 'home.html')
