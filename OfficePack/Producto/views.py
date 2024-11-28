8d0696bf5a84187049cb2b6e4f980ec8998e3599
Alba Ramos <albramvar1@alum.us.es>
2024-11-28 10:50:24 +0100

    Productos agotados y mostrar productos

--- OfficePack/Producto/views.py
+++ OfficePack/Producto/views.py
@@ -16,7 +16,7 @@ def crear_producto(request):
 
 
 def listar_productos(request):
-    productos = Producto.objects.filter(cantidad_almacen__gt=0)
+    productos = Producto.objects.all()
     return render(request, 'listar_productos.html', {'productos': productos})
 
 
